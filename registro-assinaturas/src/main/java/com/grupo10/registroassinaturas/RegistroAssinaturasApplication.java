9c97b5446222215bd4e64a33be2f5831c2dfd691
Nicholas Spolti de Souza <nicholasspoltidesouza@gmail.com>
2024-07-01 22:05:11 -0300

    a

--- registro-assinaturas/src/main/java/com/grupo10/registroassinaturas/RegistroAssinaturasApplication.java
+++ registro-assinaturas/src/main/java/com/grupo10/registroassinaturas/RegistroAssinaturasApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegistroOrcamentosApplication {
+public class RegistroAssinaturasApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RegistroOrcamentosApplication.class, args);
+        SpringApplication.run(RegistroAssinaturasApplication.class, args);
     }
 }
